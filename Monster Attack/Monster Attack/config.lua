c9253aeacaed39c4e48ac1ced609df30a946200a
AndersonLFG <andersonlfg@live.com>
2017-10-09 19:25:09 -0300

    Novas funções

---
+++ Monster Attack/Monster Attack/config.lua
@@ -0,0 +1,49 @@
+<<<<<<< HEAD
+--
+-- For more information on config.lua see the Project Configuration Guide at:
+-- https://docs.coronalabs.com/guide/basics/configSettings
+--
+
+application =
+{
+	content =
+	{
+		width = 320,
+		height = 480, 
+		scale = "letterbox",
+		fps = 60,
+		
+		--[[
+		imageSuffix =
+		{
+			    ["@2x"] = 2,
+			    ["@4x"] = 4,
+		},
+		--]]
+	},
+}
+=======
+--
+-- For more information on config.lua see the Project Configuration Guide at:
+-- https://docs.coronalabs.com/guide/basics/configSettings
+--
+
+application =
+{
+	content =
+	{
+		width = 320,
+		height = 480, 
+		scale = "letterbox",
+		fps = 60,
+		
+		--[[
+		imageSuffix =
+		{
+			    ["@2x"] = 2,
+			    ["@4x"] = 4,
+		},
+		--]]
+	},
+}
+>>>>>>> 75d7c35bac5997d344f124859aedda681bb8a5eb
---
+++ Monster Attack/Monster Attack/main.lua
@@ -0,0 +1,338 @@
+<<<<<<< HEAD
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+local composer = require( "composer")
+local widget = require "widget"
+local physics = require( "physics" )
+
+display.setStatusBar( display.HiddenStatusBar )
+system.activate("multitouch")
+
+physics.start()
+physics.setGravity( 0, 9.8 )
+
+local doLauch = false
+local scene = composer.newScene()
+local timeCount = 60
+local lives = 3
+local score = 0
+local died = false
+local livesText
+local scoreText
+
+-- Display lives and score
+livesText = display.newText( "Lives: " .. lives, 200, 80, native.systemFont, 36 )
+scoreText = display.newText( "Score: " .. score, 400, 80, native.systemFont, 36 )
+
+local background = display.newImageRect( "castleday.png", 360, 570 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+local platform = display.newImageRect( "skullfield.png", 320, 100 )
+platform.x = display.contentCenterX
+platform.y = display.contentHeight
+physics.addBody( platform, "static", {bounce=0})
+platform.objType = "ground"
+platform.myName = "platform"
+
+local naveGroup = display.newGroup()
+
+local balloon = display.newImageRect(naveGroup, "cannon.png", 109, 129 )
+--entre 40 e 280
+balloon.x = display.contentCenterX
+balloon.y = display.contentCenterY*1.65
+physics.addBody( balloon, "dynamic", { radius=10 } )
+balloon.alpha = 1.0
+balloon.myName = "cannon"
+
+function fireLaser()
+	local bullet = display.newImageRect(naveGroup, "cannonball.png", 27.6, 27.6 )
+	physics.addBody( bullet, "dynamic", { radius=10, { isSensor=true }} )
+	bullet.isBullet = true
+	bullet.x = balloon.x
+	bullet.y = balloon.y
+	bullet.myName = "bullet"
+	bullet:toBack()
+
+	transition.to(bullet, { y=-40, time=500,
+		onComplete = function() display.remove(bullet)
+			-- body
+		end})
+end
+
+local function restoreCannon()
+ 
+    balloon.isBodyActive = false
+    balloon.x = display.contentCenterX
+    balloon.y = display.contentHeight 
+ 
+    -- Fade in the cannon
+    transition.to( balloon, { alpha=1, time=4000,
+        onComplete = function()
+            balloon.isBodyActive = true
+            died = false
+        end
+    } )
+end
+
+local monstersTable = {}
+local function createMonster()
+ 
+    local newMonster = display.newImageRect( "creature orange.png", 110, 110 )
+    table.insert( monstersTable, newMonster )
+    physics.addBody( newMonster, "dynamic", { radius=40, bounce=0.8 } )
+    newMonster.myName = "monster"
+ 
+    local whereFrom = math.random( 3 )
+
+    if ( whereFrom == 1 ) then
+        -- From the left
+        newMonster.x = contentCenterX
+        newMonster.y = math.random( 500 )
+        newMonster:setLinearVelocity( math.random( 40,120 ), math.random( 20,60 ) )
+    end
+end
+
+local function gameLoop()
+ 
+    -- Create new asteroid
+    createMonster()
+ 
+        -- Remove asteroids which have drifted off screen
+    for i = #monstersTable, 1, -1 do
+        local thisMonster = monstersTable[i]
+ 
+        if ( thisMonster.x < -100 or
+             thisMonster.x > display.contentWidth + 100 or
+             thisMonster.y < -100 or
+             thisMonster.y > display.contentHeight + 100 )
+        then
+            display.remove( thisMonster )
+            table.remove( monstersTable, i )
+        end
+    end
+end
+
+gameLoopTimer = timer.performWithDelay( 500, gameLoop, 0 )
+
+local function onCollision( event )
+ 
+    if ( event.phase == "began" ) then
+ 
+        local obj1 = event.object1
+        local obj2 = event.object2
+ 		
+        if ( obj1.myName == "monster" and obj2.myName == "platform" )
+        then
+            -- Remove monster
+            display.remove( obj1 )
+        end
+
+        if ( ( obj1.myName == "bullet" and obj2.myName == "monster" ) or
+             ( obj1.myName == "monster" and obj2.myName == "bullet" ) )
+        then
+            -- Remove both the bullet and monster
+            display.remove( obj1 )
+            display.remove( obj2 )
+ 
+            for i = #monstersTable, 1, -1 do
+                if ( monstersTable[i] == obj1 or monstersTable[i] == obj2 ) then
+                    table.remove( monstersTable, i )
+                    break
+                end
+            end
+ 
+            -- Increase score
+            score = score + 100
+            scoreText.text = "Score: " .. score
+
+        elseif ( ( obj1.myName == "cannon" and obj2.myName == "monster" ) or
+                 ( obj1.myName == "monster" and obj2.myName == "cannon" ) )
+        then
+            if ( died == false ) then
+                died = true
+ 
+                -- Update lives
+                lives = lives - 1
+                livesText.text = "Lives: " .. lives
+ 
+                if ( lives == 0 ) then
+                    display.remove( balloon )
+                else
+                    balloon.alpha = 0
+                    timer.performWithDelay( 1000, restoreCannon )
+                end
+            end
+        end
+    end
+end
+
+local badMonster = display.newImageRect(naveGroup, "creature orange.png", 110, 110 )
+--entre 40 e 280
+badMonster.x = display.contentCenterX --+ 100
+badMonster.y = display.contentCenterY - 200
+badMonster.alpha = 1.0
+physics.addBody( badMonster, "static", { radius=0, bounce=0.1 } )
+
+function moveLeft()
+	if balloon.x > 40 then
+		transition.to ( balloon, { time=150, x = balloon.x - 25})
+	end
+end
+
+function moveRight()
+	if balloon.x < 280 then
+		transition.to ( balloon, { time=150, x = balloon.x + 25})
+	end	
+end
+
+--function shoot()
+--	bullet:setLinearVelocity( 0, -700)
+--end
+
+local mLeft = widget.newButton {
+	
+	--label = "Left",
+	--labelColor = { default={0,100,0}, over={255,215,0} },
+	defaultFile = "button1.png",
+	overFile = "button2.png",
+	x = display.contentWidth/7, y = display.contentHeight/1,
+	width = 80, height = 80,
+	onPress = moveLeft,
+}
+
+local mRight = widget.newButton {
+	
+	--label = "Right",
+	--labelColor = { default={0,100,0}, over={255,215,0} },
+	defaultFile = "button1.png",
+	overFile = "button2.png",
+	x = display.contentWidth/1.17, y = display.contentHeight/1,
+	width = 80, height = 80,
+	onPress = moveRight,
+}
+
+local atirar = widget.newButton {
+	
+	--label = "Right",
+	--labelColor = { default={0,100,0}, over={255,215,0} },
+	defaultFile = "atirar.png",
+	overFile = "atirar2.png",
+	x = display.contentWidth/2, y = display.contentHeight/1,
+	width = 90, height = 90,
+	onPress = fireLaser,
+	
+}
+Runtime:addEventListener( "collision", onCollision )
+
+--atirar:addEventListener("touch", fireLaser)
+=======
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+local composer = require( "composer")
+local widget = require "widget"
+local physics = require( "physics" )
+
+display.setStatusBar( display.HiddenStatusBar )
+system.activate("multitouch")
+
+physics.start()
+--physics.setGravity( 0, 9.8 )
+
+local doLauch = false
+local scene = composer.newScene()
+local timeCount = 100
+
+local background = display.newImageRect( "castleday.png", 360, 570 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+local platform = display.newImageRect( "skullfield.png", 320, 320 )
+platform.x = display.contentCenterX
+platform.y = display.contentHeight+100
+physics.addBody( platform, "static")
+
+local balloon = display.newImageRect( "cannon.png", 109, 129 )
+--entre 40 e 280
+balloon.x = display.contentCenterX
+balloon.y = display.contentCenterY*1.65
+balloon.alpha = 1.0
+--physics.addBody( balloon, "static", { radius=50, bounce=0.1 } )
+
+local bullet = display.newImageRect( "cannonball.png", 27.6, 27.6 )
+bullet.x = display.contentCenterX
+bullet.y = display.contentCenterY + 165
+physics.addBody( bullet, "dynamic", { radius=50} )
+
+local badMonster = display.newImageRect( "creature orange.png", 110, 110 )
+--entre 40 e 280
+badMonster.x = display.contentCenterX --+ 100
+badMonster.y = display.contentCenterY - 200
+badMonster.alpha = 1.0
+physics.addBody( badMonster, "static", { radius=0, bounce=0.1 } )
+
+local naveGroup = display.newGroup()
+	naveGroup:insert(balloon)
+	naveGroup:insert(bullet)
+
+--local function onCollision( event )
+--	if(event.phase == "began") then
+
+--end
+
+function moveLeft()
+	if balloon.x > 40 then
+		transition.to ( balloon, { time=150, x = balloon.x - 25})
+	end
+end
+
+function moveRight()
+	if balloon.x < 280 then
+		transition.to ( balloon, { time=150, x = balloon.x + 25})
+	end	
+end
+
+function shoot()
+	bullet:setLinearVelocity( 0, -700)
+end
+
+local mLeft = widget.newButton {
+	
+	--label = "Left",
+	--labelColor = { default={0,100,0}, over={255,215,0} },
+	defaultFile = "button1.png",
+	overFile = "button2.png",
+	x = display.contentWidth/7, y = display.contentHeight/1,
+	width = 80, height = 80,
+	onPress = moveLeft,
+}
+
+local mRight = widget.newButton {
+	
+	--label = "Right",
+	--labelColor = { default={0,100,0}, over={255,215,0} },
+	defaultFile = "button1.png",
+	overFile = "button2.png",
+	x = display.contentWidth/1.17, y = display.contentHeight/1,
+	width = 80, height = 80,
+	onPress = moveRight,
+}
+
+local atirar = widget.newButton {
+	
+	--label = "Right",
+	--labelColor = { default={0,100,0}, over={255,215,0} },
+	defaultFile = "atirar.png",
+	overFile = "atirar2.png",
+	x = display.contentWidth/2, y = display.contentHeight/1,
+	width = 90, height = 90,
+	onPress = shoot,
+}
+--atirar:addEventListener("touch", shoot)
+>>>>>>> 75d7c35bac5997d344f124859aedda681bb8a5eb
